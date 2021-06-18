4f2996baf2c235f633bc7a5656579c7e511dca77
Kellie Ryan <littlekel03@gmail.com>
2021-06-18 14:41:51 +0800

    trying to join user to holiday table

--- controllers/api/holiday-routes.js
+++ controllers/api/holiday-routes.js
@@ -2,37 +2,32 @@ const router = require("express").Router();
 const session = require("express-session");
 const sequelize = require("../../config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const Holiday = require("../../models/Holiday");
+const { Holiday, User } = require("../../models");
 
 // Get all holidays
 router.get("/", async (request, response) => {
   try {
     const holidayData = await Holiday.findAll({
-      attributes: ["id", "destination_location", "start_date", "end_date"],
+      include: [{ model: User }],
+      attributes: ["id", "username", "destination_location", "start_date", "end_date"],
     });
     response.status(200).json(holidayData);
     console.log("holidayData", holidayData);
   } catch (error) {
-    response.status(500).json(error);
+    response.status(500).json(error.message);
   }
 });
 
 // Create a new holiday
 router.post("/", async (request, response) => {
   try {
-    const holidayData = await Holiday.create({
+    const holidayData = await Holiday.create( request.body, {
       user_id: request.body.user_id,
       destination_location: request.body.destination_location,
       start_date: request.body.start_date,
       end_date: request.body.end_date,
     });
-    console.log("holiday data", holidayData);
-
-    request.session.save(() => {
-      request.body.user_id = holidayData.id;
-      request.session.loggedIn = true;
-      response.status(200).json(holidayData);
-    });
+    console.log("user_id", user_id);
   } catch (error) {
     response.status(400).json(error.message);
   }
